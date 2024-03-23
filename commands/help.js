77a5c60db8c35dcba946dcbf41c38285af093fdf
Z A I I K Y <156815062+quangminh1341@users.noreply.github.com>
2024-03-23 22:29:51 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -59,18 +59,18 @@ module.exports = {
         ) 
        .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
       const button1 = new ButtonBuilder()
-        .setLabel('YouTube')
-        .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+        .setLabel('Discord')
+        .setURL('https://discord.gg/MsNYkPPUcp')
         .setStyle(ButtonStyle.Link);
 
       const button2 = new ButtonBuilder()
-        .setLabel('Discord')
-        .setURL('https://discord.gg/FUEHs7RCqz')
+        .setLabel('NZ Service')
+        .setURL('https://ayo.so/nzservice')
         .setStyle(ButtonStyle.Link);
 
       const button3 = new ButtonBuilder()
-        .setLabel('Code')
-        .setURL('https://github.com/RTX-GAMINGG/RTX-MUSIC-BOT-v3.6')
+        .setLabel('Mọi Mọi')
+        .setURL('https://discord.gg/ahqzZyXBJu')
         .setStyle(ButtonStyle.Link);
 
       const row = new ActionRowBuilder()
