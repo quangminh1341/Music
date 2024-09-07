7277bda022728432e1f134c0181e250bd6bd9ad4
Z A I I K Y <156815062+quangminh1341@users.noreply.github.com>
2024-09-08 04:17:17 +0900

    Update musicicons.js

--- UI/icons/musicicons.js
+++ UI/icons/musicicons.js
@@ -6,7 +6,7 @@ const musicIcons = {
     pauseresumeIcon:'https://cdn.discordapp.com/emojis/836145735254540339.gif',
     playIcon:'https://cdn.discordapp.com/attachments/1230824451990622299/1236664581364125787/music-play.gif?ex=669c5e64&is=669b0ce4&hm=b081d67248271167b5aec2e07a2c9c848e16bfa5ba4bdb2067221b0d259c1b38&',
     loopIcon: 'https://cdn.discordapp.com/emojis/749272851529334795.gif',
-    beatsIcon: 'https://cdn.discordapp.com/emojis/928310693416009828.gif',
+    beatsIcon: 'https://cdn.discordapp.com/attachments/1201746680207396925/1282057007171371028/steamuserimages-a.akamaihd.gif?ex=66ddf821&is=66dca6a1&hm=7e09643fa020aa9dfd4587e32bfa94545e117d8ed7c670e3d6eb160fb099c9e0&',
     skipIcon: 'https://cdn.discordapp.com/emojis/938388856095514654.gif',
     stopIcon: 'https://cdn.discordapp.com/emojis/1021628438441902100.gif',
     volumeIcon: 'https://cdn.discordapp.com/emojis/1040824501711159397.gif',
