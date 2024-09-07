52eb32d3c270669d08062f8cd5cb5be5d1d2a3d9
Z A I I K Y <156815062+quangminh1341@users.noreply.github.com>
2024-09-08 04:08:59 +0900

    Update mpause.js

--- excesscommands/lavalink/mpause.js
+++ excesscommands/lavalink/mpause.js
@@ -15,7 +15,7 @@ module.exports = {
             .setAuthor({ 
                 name: lang.songPausedTitle, 
                 iconURL: musicIcons.pauseresumeIcon,
-                url: "https://discord.gg/xQF9f9yUEM"
+                url: "https://discord.gg/fTfamNb3mS"
             })
             .setFooter({ text: lang.footerText, iconURL: musicIcons.footerIcon })
             .setDescription(lang.songPausedDescription);
