2e1e1056df3099b5de91ba6eb21f0140e4a95026
Z A I I K Y <156815062+quangminh1341@users.noreply.github.com>
2024-12-04 21:53:49 +0900

    Update musicard.js

--- UI/banners/musicard.js
+++ UI/banners/musicard.js
@@ -6,6 +6,7 @@ const data = {
         path.resolve(__dirname, '../musicimages/2.jpg'),
         path.resolve(__dirname, '../musicimages/3.jpg'),
         path.resolve(__dirname, '../musicimages/4.jpg'),
+        path.resolve(__dirname, '../musicimages/5.jpg'),
     ],
 };
 
